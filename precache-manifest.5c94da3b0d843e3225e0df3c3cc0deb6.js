ebb8f9661caf9d4aa7c3dcc87558fab1c751105b
e8jnayem <jannaten.nayem@eng.tamk.fi>
2019-10-25 00:35:31 +0300

    Updates

---
+++ precache-manifest.5c94da3b0d843e3225e0df3c3cc0deb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00118fee721ada809dec3e95954eb3a0",
+    "url": "/mosnter-rolodex/index.html"
+  },
+  {
+    "revision": "09476f6c20aee8d78775",
+    "url": "/mosnter-rolodex/static/css/main.b2ea8435.chunk.css"
+  },
+  {
+    "revision": "8ae996a654f27ebbabb8",
+    "url": "/mosnter-rolodex/static/js/2.2c8a6238.chunk.js"
+  },
+  {
+    "revision": "09476f6c20aee8d78775",
+    "url": "/mosnter-rolodex/static/js/main.90d6faea.chunk.js"
+  },
+  {
+    "revision": "c1d8e5fb86989b631ba7",
+    "url": "/mosnter-rolodex/static/js/runtime~main.58af9df8.js"
+  }
+]);
